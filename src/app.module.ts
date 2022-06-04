077ba09d28a6779318b790f8d67a4e9454297848
Wonyoung Kang <onezerokang@gmail.com>
2022-06-04 15:30:19 +0900

    초기 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsersController } from './users/users.controller';
+import { UsersService } from './users/users.service';
+import { RecordsController } from './records/records.controller';
+import { RecordsService } from './records/records.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [UsersController, RecordsController],
+  providers: [UsersService, RecordsService],
 })
 export class AppModule {}
